a8431dbdacf5b57bc77f25a2dd5a0d3b4aa07932
PapermasteroneGH <papermasteronegh@gmail.com>
2026-03-20 01:18:59 +0700

    Switch to HashRouter for GitHub Pages/Vercel routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react'
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 const services = [
